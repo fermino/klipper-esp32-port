8a95be9f634994608e1bc0e21bb2df542bd75dc0
Fermín Olaiz <ferminolaiz@gmail.com>
2025-11-16 06:32:53 -0300

    gpio: cleanup enumeration

--- src/esp32/gpio/gpio.c
+++ src/esp32/gpio/gpio.c
@@ -1,6 +1,4 @@
 #include "gpio/gpio.h"
 #include "command.h"
-#include "hal/gpio_ll.h"
 
-// @todo está ok? ._. Num max o num -1?
-DECL_ENUMERATION_RANGE("pin", "GPIO_NUM_0", GPIO_NUM_0, GPIO_NUM_MAX);
+DECL_ENUMERATION_RANGE("pin", "IO0", GPIO_NUM_0, GPIO_NUM_MAX);
