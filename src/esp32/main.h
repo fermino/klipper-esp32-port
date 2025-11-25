6cc34923845abf6726807230d1f1466cfb9c71a7
Fermín Olaiz <ferminolaiz@gmail.com>
2025-11-25 18:47:24 -0300

    main: misc code style fixes

--- src/esp32/main.h
+++ src/esp32/main.h
@@ -3,7 +3,7 @@
 #include "sdkconfig.h"
 
 #if (!defined(CONFIG_FREERTOS_UNICORE) || !CONFIG_FREERTOS_UNICORE)
-    #error Klipper needs CONFIG_FREERTOS_UNICORE enabled
+#    error Klipper needs CONFIG_FREERTOS_UNICORE enabled
 #endif
 
 void app_main();
