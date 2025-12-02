1830528b214efba317e641788cb81399979f56f6
Fermín Olaiz <ferminolaiz@gmail.com>
2025-12-02 20:28:14 -0300

    cleanup unused headers

--- src/esp32/gpio.h
+++ src/esp32/gpio.h
@@ -1,32 +1,7 @@
 #pragma once
 
-#include <stdint.h>
-
-// ReSharper disable CppUnusedIncludeDirective
 #include "gpio/gpio.h"
 #include "gpio/gpio_out.h"
 #include "gpio/gpio_in.h"
 #include "gpio/gpio_pwm.h"
 #include "gpio/gpio_adc.h"
-// ReSharper restore CppUnusedIncludeDirective
-
-/**
- * @todo OTHERSADASDASDS
- */
-
-struct spi_config {
-    uint8_t spcr, spsr;
-};
-struct spi_config spi_setup(uint32_t bus, uint8_t mode, uint32_t rate);
-void spi_prepare(struct spi_config config);
-void spi_transfer(struct spi_config config, uint8_t receive_data
-                  , uint8_t len, uint8_t *data);
-
-struct i2c_config {
-    uint8_t addr;
-};
-
-struct i2c_config i2c_setup(uint32_t bus, uint32_t rate, uint8_t addr);
-int i2c_write(struct i2c_config config, uint8_t write_len, uint8_t *write);
-int i2c_read(struct i2c_config config, uint8_t reg_len, uint8_t *reg
-              , uint8_t read_len, uint8_t *read);
